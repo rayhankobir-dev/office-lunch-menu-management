540818cd9b3b960c6326688196532b6eabc092a3
rayhankobir-dev <rayhankobir793@gmail.com>
2024-05-25 19:09:20 +0600

    feat: add session middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,14 +3,36 @@ import path from "path";
 import express from "express";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { corsConfig, environment } from "./config.js";
+import { corsConfig, environment, sessionConfig } from "./config.js";
 import routes from "./routes/index.js";
 import ApiError from "./helpers/ApiError.js";
+import pgSession from "connect-pg-simple";
+import session from "express-session";
+import { client } from "./helpers/db.js";
+
+// creating postgres session with express session
+const PgSession = pgSession(session);
 
 // defining express app
 const app = express();
 
 // configuring middleware
+app.use(
+  session({
+    store: new PgSession({
+      pool: client,
+      tableName: "session",
+    }),
+    secret: sessionConfig.secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: environment !== "dev",
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 app.use(express.json());
 app.use(cors(corsConfig));
 app.use(cookieParser());
